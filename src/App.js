dd06cd0121c2d7c3bfb610502ccb1b57a2f754db
Bayartsengel Battulga <tsengelbattulga1227@gmail.com>
2021-08-20 10:15:47 -0700

    Completed Core Deliverables

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import BillCollection from './components/BillCollection';
 import BillsCast from './components/BillsCast';
 
 export default function App() {
   //start here with your code for step one
 
+  const [bills, setBills] = useState([])
+
+
+  useEffect(() => {
+    fetch('http://localhost:8002/bills')
+    .then((r) => r.json())
+    .then((bills)=> setBills(bills))
+  }, []);
+
+  function enlistBill(id) {
+    setBills(
+      bills.map((bill) => (bill.id === id ? {...bill, enlisted: true} : bill))
+    );
+  }
+
+  function releaseBill(id) {
+    setBills(
+      bills.map((bill) => (bill.id === id ? {...bill, enlisted: false} : bill))
+    );
+  }
+
+  function dischargeBill(id) {
+    setBills(bills.filter((bill) => bill.id !== id));
+  }
+
   return (
     <div>
-      <BillsCast />
-      <BillCollection />
+      <BillsCast bills={bills.filter((bill) => bill.enlisted)} handleClick={releaseBill} dischargeBill={dischargeBill} />
+      <BillCollection bills={bills} handleClick={enlistBill} dischargeBill={dischargeBill} />
     </div>
   );
 }
--- src/components/BillCollection.js
+++ src/components/BillCollection.js
@@ -1,12 +1,18 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
+import BillCard from './BillCard'
 
-export default function BillCollection() {
+
+export default function BillCollection({bills, handleClick, dischargeBill}) {
   // Your code here
+  
 
   return (
     <div className="ui four column grid">
       <div className="row">
-        {/*...and here..*/}
+        {bills.map((bill) => (
+          <BillCard key={bill.id} bill={bill} handleClick={handleClick} dischargeBill={dischargeBill}/>
+        ) )}
+       
       </div>
     </div>
   );
