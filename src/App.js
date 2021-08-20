c4330cfcdeef9028bf704a8dc0f0c2a93f120304
Bayartsengel Battulga <tsengelbattulga1227@gmail.com>
2021-08-20 11:21:51 -0700

    Completed Core, Delete

--- src/App.js
+++ src/App.js
@@ -4,12 +4,41 @@ import BillsCast from './components/BillsCast';
 
 export default function App() {
   //start here with your code for step one
+  const API = 'http://localhost:8002/bills'
+  const headers = {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  };
+
 
   const [bills, setBills] = useState([])
 
+//  function postBills (id, bill){
+//    fetch(`${API}/${id}`, {
+//      method: 'POST',
+//      body: JSON.stringify(bill, id),
+//      headers,
+//    })
+//    .then((r) => r.json())
+//    .then((bills)=> setBills(bills.map(bill => (bill.id === id ? {...bill, enlisted: true} : bill) )))
+//  }
+
+function dischargeBill(id) {
+  fetch(`${API}/${id}`, {
+    method: 'DELETE',
+    
+    headers,
+   
+    }) 
+    .then(() => setBills(bills.filter((bill) => bill.id !== id) ) )
+  
+  
+}
+
+
 
   useEffect(() => {
-    fetch('http://localhost:8002/bills')
+    fetch(API)
     .then((r) => r.json())
     .then((bills)=> setBills(bills))
   }, []);
@@ -26,9 +55,9 @@ export default function App() {
     );
   }
 
-  function dischargeBill(id) {
-    setBills(bills.filter((bill) => bill.id !== id));
-  }
+  // function dischargeBill(id) {
+  //   setBills(bills.filter((bill) => bill.id !== id));
+  // }
 
   return (
     <div>
